4f485dfced6f801792e82b529f4e89259678e622
Dhruv Kumar Sharma <dhruv.kccsw@kccitm.edu.in>
2026-02-11 13:34:04 +0530

    removed unnecessary files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/login");
+  redirect("/admin/dashboard");
   return null; // This won't render anything since the user is redirected
 }
